47cbe8dcfb6c6c4dde2ed05c1b2ec0bc2f980875
HanJaehee <38396374+HanJaehee@users.noreply.github.com>
2020-10-09 20:15:12 +0900

    Update etherscan_test.js

--- backend/etherscan_test.js
+++ backend/etherscan_test.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const util = require('util');
 
-const myApi = 'DI2QYXMJ7U1UY4G8AAE175TC33B3V3SGSE';
+const myApi = '';
 const contractaddress = "0x2727b026EdB116B20196a1abF32e0cA8311E93e2";
 
 function getHistory(address){ //제조사 생성내역, 거래내역 조회
